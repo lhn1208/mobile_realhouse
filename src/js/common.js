3fc32bcc6c2af47115b17ecf36668eafb7619728
DESKTOP-ERBM92G\HANA <lhn1208@naver.com>
2019-01-15 23:15:47 +0900

    no message

--- src/js/common.js
+++ src/js/common.js
@@ -2,15 +2,23 @@ $(document).ready(function(){
     //알림창
     alarm_show();
     function alarm_show(){
-    $('.layer_alarm').animate({
-        bottom:'8%'
-    },500)
+        $('.layer_alarm').animate({
+            bottom:'8%'
+        },500)
     }
     setTimeout(alarm_remove,1000);
     function alarm_remove(){
-    $('.layer_alarm').animate({
-        bottom:'-8%'
-    },500)
+        $('.layer_alarm').animate({
+            bottom:'-8%'
+        },500)
     }
-  
+    var $pw_input=$(".member_form .pw_input")
+    var $placeholder;
+    $pw_input.on('input',function(e){
+        $placeholder= $(this).siblings(".placeholder");
+        $placeholder.css("display","none")
+        if($(this).val()==""){
+            $placeholder.css("display","block")
+        }
+    });
 });
