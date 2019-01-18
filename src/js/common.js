4e6f7eabac6064e39194cd98ed511173ac8bab3f
MODETOUR\lhn1208 <lhn1208@modetour.com>
2019-01-18 14:55:14 +0900

    no message

--- src/js/common.js
+++ src/js/common.js
@@ -12,13 +12,21 @@ $(document).ready(function(){
             bottom:'-8%'
         },500)
     }
-    var $pw_input=$(".member_form .pw_input")
+    var $pw_input=$('.member_form .pw_input')
     var $placeholder;
     $pw_input.on('input',function(e){
-        $placeholder= $(this).siblings(".placeholder");
-        $placeholder.css("display","none")
+        $placeholder= $(this).siblings('.placeholder');
+        $placeholder.css('display','none')
         if($(this).val()==""){
-            $placeholder.css("display","block")
+            $placeholder.css('display','block')
         }
     });
+
+    var $member_input=$('.member_form .member_input');
+    $member_input.focus('input',function(){
+        $(this).next('.delete').css('display','block');
+    })
+    $member_input.blur('input',function(){
+        $(this).next('.delete').removeAttr('style');
+    })
 });
