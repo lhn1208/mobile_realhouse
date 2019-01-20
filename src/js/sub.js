e004d2ae8b03acd5ba93f6faf7060fa377cebea9
DESKTOP-ERBM92G\HANA <lhn1208@naver.com>
2019-01-20 22:56:26 +0900

    no message

--- src/js/sub.js
+++ src/js/sub.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var $Slidebox=$('.slide_box');
     var $all_offer=$('.slide_box.slide_offer');//전체매물
     var $area_offer=$('.area_offer');//지역매물
-    //var $noBasic=$('.slide_box');//지역매물
+    var $noBasic=$('.slide_box');//지역매물
     $Slidebox.find('.header').click(function(){
         if(!($Slidebox.hasClass('animate_h_up'))){
             $Slidebox.removeClass('animate_h_down').addClass('animated').addClass('animate_h_up');
@@ -15,8 +15,8 @@ $(document).ready(function(){
 
     //지역매물
     $area_offer.on('touchmove',function(){
-        if(!($Slidebox.hasClass('animate_h_up'))){
-             $Slidebox.removeClass('animate_h_down').addClass('animated').addClass('animate_h_up');
+        if(!($area_offer.hasClass('animate_h_up'))){
+             $area_offer.removeClass('animate_h_down').addClass('animated').addClass('animate_h_up');
         }
     });
 
