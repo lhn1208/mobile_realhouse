56513b4a2f7b54a619ac8a7ee9856c21a433af83
DESKTOP-ERBM92G\HANA <lhn1208@naver.com>
2019-02-02 18:03:12 +0900

    no message

--- src/js/common.js
+++ src/js/common.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     })
 
     //swiper menu
-    var top_h=$(".present_sec").outerHeight() + 54;
+    var top_h=$(".present_sec").outerHeight() + 34;
     var $swiperMenu=$('.swiper_fix .swiper_menu');
     $(window).scroll(function() {
         var sct=$(window).scrollTop();
